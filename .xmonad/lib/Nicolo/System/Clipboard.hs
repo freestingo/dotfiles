50fae6e1c2799bfb92fe9e88291210014753eed0
Nicolo Traini <nicolo.traini@oncode.it>
2021-09-06 00:26:12 +0200

    Add custom clipboard haskell function to xmonad config

---
+++ .xmonad/lib/Nicolo/System/Clipboard.hs
@@ -0,0 +1,12 @@
+module Nicolo.System.Clipboard where
+
+import System.Process
+import System.IO (hPutStr, hClose)
+
+setClipboardString :: String -> IO ()
+setClipboardString str = do
+    (Just inp, _, _, _) <- createProcess
+                             (proc "xclip" ["-selection", "clipboard"])
+                             { std_in = CreatePipe }
+    hPutStr inp str
+    hClose inp
